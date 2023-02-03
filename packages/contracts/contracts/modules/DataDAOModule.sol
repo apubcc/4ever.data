b5295f4a81d6ca78f43ddfe4f0526acfd4954c9f
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-02-04 07:41:03 +0800

    feat: init interface for data dao modules

---
+++ packages/contracts/contracts/modules/DataDAOModule.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import "../IERC677.sol";
+import "../MemberLeaveCode.sol";
+
+interface IDataDAO {
+    function owner() external returns (address);
+
+    function removeMember(address member, MemberLeaveCode leaveCode) external;
+
+    function addMember(address newMember) external;
+
+    function isMember(address member) external view returns (bool);
+
+    function isMembershipManager(address manager) external view returns (bool);
+}
+
+contract DataDAOModule {
+    address public dataDAO;
+
+    modifier onlyOwner() {
+        require(msg.sender == IDataDAO(dataDAO).owner(), "error_onlyOwner");
+        _;
+    }
+
+    modifier onlyMembershipManager() {
+        require(
+            IDataDAO(dataDAO).isMembershipManager(msg.sender),
+            "error_onlyMembershipManager"
+        );
+        _;
+    }
+
+    modifier onlyDataDAO() {
+        require(msg.sender == dataDAO, "error_onlyDataDAOContract");
+        _;
+    }
+
+    constructor(address dataDAOAddress) {
+        dataDAO = dataDAOAddress;
+    }
+}
