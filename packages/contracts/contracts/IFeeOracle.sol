72dbb97d74e1a5201bda2f67de3c278db3ba8cda
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-02-04 03:54:06 +0800

    feat: init interface for protocol fee oracle

---
+++ packages/contracts/contracts/IFeeOracle.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+interface IFeeOracle {
+    // TODO: fee is a percentage, not absolute wei, rename it to feePercentageWei or similar
+    function protocolFeeFor(address dataUnion) external view returns(uint feeWei);
+    function beneficiary() external view returns(address);
+}
