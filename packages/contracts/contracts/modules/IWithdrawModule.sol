b2d3b6e41338c1bfb038754e987dc8cd104e1599
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-02-04 06:29:21 +0800

    feat: init module for data owner to withdraw funds

---
+++ packages/contracts/contracts/modules/IWithdrawModule.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import "../IERC677.sol";
+
+interface IWithdrawModule {
+    /**
+     * When a withdraw happens in the DataDAO, tokens are transferred to the withdrawModule, then onWithdraw function is called.
+     * The withdrawModule is then free to manage those tokens as it pleases.
+     */
+    function onWithdraw(address member, address to, IERC677 token, uint amountWei) external;
+
+    /**
+     * WithdrawModule can also set limits to withdraws between 0 and (earnings - previously withdrawn earnings).
+     */
+    function getWithdrawLimit(address member, uint maxWithdrawable) external view returns (uint256);
+}
