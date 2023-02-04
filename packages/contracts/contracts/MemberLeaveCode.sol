cc82a6ecb9ab1cb0b3cb9818dcd92f3f69c182f5
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-02-05 00:20:58 +0800

    docs: update comment

--- packages/contracts/contracts/MemberLeaveCode.sol
+++ packages/contracts/contracts/MemberLeaveCode.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.9;
  * Describes how the DataDAO member left
  */
 enum MemberLeaveCode {
-    SELF,   // self remove using partMember()
-    AGENT,  // removed by joinPartAgent using partMember()
-    BANNED  // removed by BanModule
+    SELF, // self remove using partMember()
+    AGENT, // removed by membership manager using partMember()
+    BANNED // removed by BanModule
 }
