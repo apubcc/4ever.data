6c17dc943c97401d2986656d0ee83fa3aa69f84b
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-02-06 05:26:14 +0800

    feat: init token for 4ever.data

---
+++ packages/contracts/contracts/FourEverDataUSDC.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDC is ERC20, Ownable {
+    constructor() ERC20("4Ever.Data USDC", "USDC") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
