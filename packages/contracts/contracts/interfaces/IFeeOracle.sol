f7083fb4effb0b54422fc1395caa7f7ea36c4ed4
apubcc-admin <apublockchaincryptoclub@gmail.com>
2023-02-06 12:06:28 +0800

    fix: update solidity version

--- packages/contracts/contracts/interfaces/IFeeOracle.sol
+++ packages/contracts/contracts/interfaces/IFeeOracle.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 interface IFeeOracle {
     // TODO: fee is a percentage, not absolute wei, rename it to feePercentageWei or similar
