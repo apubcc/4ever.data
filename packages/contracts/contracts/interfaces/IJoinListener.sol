1b59bfec9ce9fd468f0f212e075bb5abb25c0158
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-02-06 11:31:41 +0800

    fix: remove unused .sol files to reduce contract code size

--- packages/contracts/contracts/interfaces/IJoinListener.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.9;
-
-interface IJoinListener {
-    function onJoin(address newMember) external;
-}
--- packages/contracts/contracts/interfaces/ILeaveListener.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.9;
-
-import "../MemberLeaveCode.sol";
-
-interface ILeaveListener {
-    function onLeave(address leavingMember, MemberLeaveCode leaveCode) external;
-}
